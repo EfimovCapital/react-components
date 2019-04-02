415be6dfb5d5e4307f9df28459b6787c78f2b0cb
Negaex <gh@negaex.com>
2019-04-03 09:35:20 +1100

    Updated examples preview

--- src/selectMarket/Example.tsx
+++ src/selectMarket/Example.tsx
@@ -1,92 +1,94 @@
-import * as React from "react";
 
-import { SelectMarket } from "./SelectMarket";
+export const name = "Example";
+// import * as React from "react";
 
-import { OrderedMap } from "immutable";
+// import { SelectMarket } from "./SelectMarket";
 
-type Token = string;
-type MarketPair = string;
+// import { OrderedMap } from "immutable";
 
-export const Tokens = new Map<Token, { symbol: string; name: string }>()
-  .set("DAI", { symbol: "DAI", name: "Dai" })
-  .set("BTC", { symbol: "BTC", name: "Bitcoin" })
-  .set("ETH", { symbol: "ETH", name: "Ethereum" })
-  .set("REN", { symbol: "REN", name: "Ren" })
-  .set("TUSD", { symbol: "TUSD", name: "TrueUSD" })
-  .set("WBTC", { symbol: "WBTC", name: "Wrapped Bitcoin" });
+// type Token = string;
+// type MarketPair = string;
 
-export const MarketPairs = OrderedMap<
-  MarketPair,
-  { symbol: string; quote: string; base: string }
->()
-  .set("BTC/DAI", { symbol: "BTC/DAI", quote: "DAI", base: "BTC" })
-  .set("ETH/DAI", { symbol: "ETH/DAI", quote: "DAI", base: "ETH" })
-  .set("ETH/BTC", { symbol: "ETH/BTC", quote: "BTC", base: "ETH" })
-  .set("WBTC/BTC", { symbol: "WBTC/BTC", quote: "BTC", base: "WBTC" });
+// export const Tokens = new Map<Token, { symbol: string; name: string }>()
+//   .set("DAI", { symbol: "DAI", name: "Dai" })
+//   .set("BTC", { symbol: "BTC", name: "Bitcoin" })
+//   .set("ETH", { symbol: "ETH", name: "Ethereum" })
+//   .set("REN", { symbol: "REN", name: "Ren" })
+//   .set("TUSD", { symbol: "TUSD", name: "TrueUSD" })
+//   .set("WBTC", { symbol: "WBTC", name: "Wrapped Bitcoin" });
 
-export const getMarket = (
-  left: Token,
-  right: Token
-): MarketPair | undefined => {
-  return (
-    MarketPairs.findKey(
-      marketDetails =>
-        marketDetails.base === left && marketDetails.quote === right
-    ) ||
-    MarketPairs.findKey(
-      marketDetails =>
-        marketDetails.base === right && marketDetails.quote === left
-    ) ||
-    undefined
-  );
-};
+// export const MarketPairs = OrderedMap<
+//   MarketPair,
+//   { symbol: string; quote: string; base: string }
+// >()
+//   .set("BTC/DAI", { symbol: "BTC/DAI", quote: "DAI", base: "BTC" })
+//   .set("ETH/DAI", { symbol: "ETH/DAI", quote: "DAI", base: "ETH" })
+//   .set("ETH/BTC", { symbol: "ETH/BTC", quote: "BTC", base: "ETH" })
+//   .set("WBTC/BTC", { symbol: "WBTC/BTC", quote: "BTC", base: "WBTC" });
 
-export class SelectMarketExample extends React.Component<
-  {},
-  { top: string; bottom: string }
-  > {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      top: "BTC",
-      bottom: "DAI"
-    };
-  }
+// export const getMarket = (
+//   left: Token,
+//   right: Token
+// ): MarketPair | undefined => {
+//   return (
+//     MarketPairs.findKey(
+//       marketDetails =>
+//         marketDetails.base === left && marketDetails.quote === right
+//     ) ||
+//     MarketPairs.findKey(
+//       marketDetails =>
+//         marketDetails.base === right && marketDetails.quote === left
+//     ) ||
+//     undefined
+//   );
+// };
 
-  /**
-   * The main render function.
-   * @dev Should have minimal computation, loops and anonymous functions.
-   */
-  public render(): React.ReactNode {
-    return (
-      <div className="select-market--example">
-        <SelectMarket
-          top={true}
-          thisToken={this.state.top}
-          otherToken={this.state.bottom}
-          allTokens={Tokens}
-          key={"top"}
-          onChange={this.onTopChange}
-          getMarket={getMarket}
-        />
-        <SelectMarket
-          top={false}
-          thisToken={this.state.bottom}
-          otherToken={this.state.top}
-          allTokens={Tokens}
-          key={"bottom"}
-          onChange={this.onBottomChange}
-          getMarket={getMarket}
-        />
-      </div>
-    );
-  }
+// export default class SelectMarketExample extends React.Component<
+//   {},
+//   { top: string; bottom: string }
+//   > {
+//   constructor(props: {}) {
+//     super(props);
+//     this.state = {
+//       top: "BTC",
+//       bottom: "DAI"
+//     };
+//   }
 
-  private readonly onTopChange = (token: string): void => {
-    this.setState({ top: token });
-  };
+//   /**
+//    * The main render function.
+//    * @dev Should have minimal computation, loops and anonymous functions.
+//    */
+//   public render(): React.ReactNode {
+//     return (
+//       <div className="select-market--example">
+//         <SelectMarket
+//           top={true}
+//           thisToken={this.state.top}
+//           otherToken={this.state.bottom}
+//           allTokens={Tokens}
+//           key={"top"}
+//           onChange={this.onTopChange}
+//           getMarket={getMarket}
+//         />
+//         <SelectMarket
+//           top={false}
+//           thisToken={this.state.bottom}
+//           otherToken={this.state.top}
+//           allTokens={Tokens}
+//           key={"bottom"}
+//           onChange={this.onBottomChange}
+//           getMarket={getMarket}
+//         />
+//       </div>
+//     );
+//   }
 
-  private readonly onBottomChange = (token: string): void => {
-    this.setState({ bottom: token });
-  };
-}
+//   private readonly onTopChange = (token: string): void => {
+//     this.setState({ top: token });
+//   };
+
+//   private readonly onBottomChange = (token: string): void => {
+//     this.setState({ bottom: token });
+//   };
+// }
---
+++ src/tokenIcon/examples/tokenIcon.example.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import { TokenIcon } from "../TokenIcon";
+
+export default () => [
+    <p><TokenIcon token={"BTC"} /> BTC</p>,
+    <p><TokenIcon token={"DAI"} /> DAI</p>,
+    <p><TokenIcon token={"DGX"} /> DGX</p>,
+    <p><TokenIcon token={"ETH"} /> ETH</p>,
+    <p><TokenIcon token={"OMG"} /> OMG</p>,
+    <p><TokenIcon token={"REN"} /> REN</p>,
+    <p><TokenIcon token={"TUSD"} /> TUSD</p>,
+    <p><TokenIcon token={"WBTC"} /> WBTC</p>,
+    <p><TokenIcon token={"ZEC"} /> ZEC</p>,
+    <p><TokenIcon token={"ZRX"} /> ZRX</p>,
+];
