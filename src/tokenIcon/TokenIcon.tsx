cb86d11dc1a34030ca6edbb98316333ab7cda3b2
Noah I <gh@negaex.com>
2019-04-02 13:43:58 +1100

    Load token icons as React components

--- src/tokenIcon/TokenIcon.tsx
+++ src/tokenIcon/TokenIcon.tsx
@@ -1,55 +1,64 @@
 import * as React from "react";
 
-import Info from "../infoLabel/info.svg";
-import BTC from "./icons/btc.svg";
-import DAI from "./icons/dai.svg";
-import DGX from "./icons/dgx.svg";
-import ETH from "./icons/eth.svg";
-import OMG from "./icons/omg.svg";
-import REN from "./icons/ren.svg";
-import TUSD from "./icons/tusd.svg";
-import WBTC from "./icons/wbtc.svg";
-import ZEC from "./icons/zec.svg";
-import ZRX from "./icons/zrx.svg";
+import { ReactComponent as Info } from "../infoLabel/info.svg";
+import { ReactComponent as BTC } from "./icons/btc.svg";
+import { ReactComponent as DAI } from "./icons/dai.svg";
+import { ReactComponent as DGX } from "./icons/dgx.svg";
+import { ReactComponent as ETH } from "./icons/eth.svg";
+import { ReactComponent as OMG } from "./icons/omg.svg";
+import { ReactComponent as REN } from "./icons/ren.svg";
+import { ReactComponent as TUSD } from "./icons/tusd.svg";
+import { ReactComponent as WBTC } from "./icons/wbtc.svg";
+import { ReactComponent as ZEC } from "./icons/zec.svg";
+import { ReactComponent as ZRX } from "./icons/zrx.svg";
 
 import "./styles.scss";
 
 const icons = {
-    BTC: BTC,
-    DAI: DAI,
-    DGX: DGX,
-    ETH: ETH,
-    OMG: OMG,
-    REN: REN,
-    TUSD: TUSD,
-    WBTC: WBTC,
-    ZEC: ZEC,
-    ZRX: ZRX,
+  BTC: BTC,
+  DAI: DAI,
+  DGX: DGX,
+  ETH: ETH,
+  OMG: OMG,
+  REN: REN,
+  TUSD: TUSD,
+  WBTC: WBTC,
+  ZEC: ZEC,
+  ZRX: ZRX
 };
 
 export class TokenIcon extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
+  constructor(props: Props) {
+    super(props);
+  }
+
+  public render = (): JSX.Element => {
+    const { token, className } = this.props;
+    let image;
+    if (icons[token]) {
+      // tslint:disable-next-line: non-literal-require
+      image = icons[token];
+    } else {
+      image = Info;
     }
 
-    public render = (): JSX.Element => {
-        const { token, className } = this.props;
-        let image;
-        if (icons[token]) {
-            // tslint:disable-next-line: non-literal-require
-            image = icons[token];
-        } else {
-            image = Info;
-        }
-
-        return <img alt="" role="presentation" className={`token--icon ${className ? className : ""}`} src={image} />;
-    }
+    return React.createElement(image, {
+      className: `token--icon ${className ? className : ""}`
+    });
+    // return (
+    //   <img
+    //     alt=""
+    //     role="presentation"
+    //     className={`token--icon ${className ? className : ""}`}
+    //     src={image}
+    //   />
+    // );
+  };
 }
 
 interface Props {
-    token: string;
-    className?: string;
+  token: string;
+  className?: string;
 }
 
-interface State {
-}
+interface State {}
