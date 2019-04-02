c43d2bdfedf2c40a720a5e2d2e2c25058d9f843b
Negaex <gh@negaex.com>
2019-04-02 14:05:08 +1100

    Seperated lib.ts and index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,3 @@
-export { Blocky } from "./blocky/Blocky";
-export { Console } from "./console/Console";
-export {
-  CurrencyIcon,
-  Currency,
-  currencies
-} from "./currencyIcon/CurrencyIcon";
-export { InfoLabel, LabelLevel } from "./infoLabel/InfoLabel";
-export { Loading } from "./loading/Loading";
-export { SearchField } from "./searchField/SearchField";
-export { TokenIcon } from "./tokenIcon/TokenIcon";
-export { SelectMarket } from "./selectMarket/SelectMarket";
-
 import * as React from "react";
 import { render } from "react-dom";
 
